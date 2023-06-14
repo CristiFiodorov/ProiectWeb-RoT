52d54b490e61d710d1f3ab9d51f96d045b39e843
Alex Neagu <nalex4080@gmail.com>
2023-06-15 01:35:43 +0300

    Changed the expirationTime of accessToken to 24 hours

--- rest-api/services/auth-service.js
+++ rest-api/services/auth-service.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 
 
 function generateAccessToken(user) {
-    return jwt.sign({ "id": user._id.toString() }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+    return jwt.sign({ "id": user._id.toString() }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' })
 }
 
 function verifyToken(req, res, next) {
