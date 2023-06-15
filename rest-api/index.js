6f570420b7c37214e39dbb2988075a8ad26613f6
Vamanu Petru <petrisor_vmy@yahoo.com>
2023-06-15 20:17:34 +0300

    Tests Create & Read

--- rest-api/index.js
+++ rest-api/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 const { ServerManager } = require('./config/server-manager');
 const {  findAllQuestions, findQuestionById, createQuestion, deleteQuestion, patchQuestion } = require('./controllers/questions-controller');
+const { createTest, findAllTests, findTestById } = require('./controllers/test-controller');
 
 const server = new ServerManager();
 
@@ -21,6 +22,10 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 // server.post('/login', async(req, res) => {
 
 // });
+//TODO case when entity not found
+server.post('/tests', async (req, res, params) => { createTest(req, res, params); });
+server.get('/tests', async (req, res, params) => { findAllTests(req, res, params); });
+server.get('/tests/:id', async (req, res, params) => { findTestById(req, res, params); });
 
 server.post('/questions', async (req, res, params) => { createQuestion(req, res, params); });
 server.patch('/questions/:id', async (req, res, params) => { patchQuestion(req, res, params); });
